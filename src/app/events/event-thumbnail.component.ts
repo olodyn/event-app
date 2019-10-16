ceb48ff0bf96bc377f8aba7ec78f8588f6e2b9a0
olodyn <ndolo.josephk@gmail.com>
2019-10-16 06:23:26 +0300

    Phase One

---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-event-thumbnail',
+  templateUrl: './event-thumbnail.component.html',
+  styleUrls: ['./event-thumbnail.component.css']
+})
+export class EventThumbnailComponent implements OnInit {
+
+  @Input() event: any 
+  //@Output() eventClick = new EventEmitter()
+
+  //handleClickMe() {
+    //this.eventClick.emit(this.event.name);
+  //}
+  logNoma(){
+    console.log("Sisis ni wanoma")
+  }
+  someProperty:any = "Some property Value"
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-events-list',
+  templateUrl: './events-list.component.html',
+  styleUrls: ['./events-list.component.css']
+})
+export class EventsListComponent implements OnInit {
+
+  event1 = {
+    id:1,
+    name: 'Angular Connect',
+    date: '9/26/2019',
+    time: '10:00 am',
+    price: 599.99,
+    imageUrl: '/assets/images/angularconnect-shield.png',
+    location: {
+      address: '1057 DT',
+      city: 'London',
+      country: 'England'
+    }
+  }
+
+  //handleEventClicked(data){
+    //console.log('received',data)
+  //}
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
