538a0113d0958168722195b0250e53c99ec2583d
olodyn <ndolo.josephk@gmail.com>
2019-10-18 05:10:45 +0300

    Third Commit

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -9,11 +9,11 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class EventThumbnailComponent implements OnInit {
 
   @Input() event: any;
-  //@Output() eventClick = new EventEmitter()
+  // @Output() eventClick = new EventEmitter()
 
-  //handleClickMe() {
-    //this.eventClick.emit(this.event.name);
-  //}
+  // handleClickMe() {
+    // this.eventClick.emit(this.event.name);
+  // }
 
   // getStartTimeClass() {
 // const isEarlyStart = this.event && this.event.time === '8:00 am'; return { green: isEarlyStart, bold: isEarlyStart }
@@ -22,18 +22,19 @@ export class EventThumbnailComponent implements OnInit {
   // return ''
   // }
 
-  getStartTimeStyle() {
-    if( this.event && this.event.time === '8:00 am' )
-        return {color: '#003300', 'font-weight': 'bold'}
-  }
-  logNoma(){
-    console.log('Sisis ni wanoma');
-  }
-  someProperty:any = 'Some property Value';
+  getStartTimeStyle(): any {
+    if ( this.event && this.event.time === '8:00 am' )
+        return  { color: '#003300', 'font-weight': 'bold' }
+    return [];
+        }
+
+
 
   constructor() { }
 
   ngOnInit() {
   }
+
+
 
 }
